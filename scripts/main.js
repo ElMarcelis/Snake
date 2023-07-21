9fbf23e15dacf362e8df5c0728f13461b2062dd2
mmoran <marcelomoran@visualmedica.com>
2023-07-21 11:47:57 -0300

    joystick

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,9 @@
+// control de celocidad con slider de 1 a 100
+//on/off IA en controls canvas
+//score en canvas
+//mosedown muestra la flecja de direccion resaltada
+//mosuse up resetea todas las flechitras
+
 let blockSize = 100
 let gameSpeed = 1000 / 5
 
@@ -53,6 +59,10 @@ let buttonDown = new Path2D()
 let buttonUp = new Path2D()
 let buttonRight = new Path2D()
 
+let arrowLeft = new Path2D()
+let arrowDown = new Path2D()
+let arrowUp = new Path2D()
+let arrowRight = new Path2D()
 window.onload = function () {
   // Set board height and width
   board = document.getElementById('board')
@@ -66,12 +76,14 @@ window.onload = function () {
   board.width = blockSize * boardRows
   console.log('board.height', board.height)
   context = board.getContext('2d')
-
   controls = document.getElementById('controls')
   controls.height = 4 * blockSize
   controls.width = 4 * blockSize
   contextControls = controls.getContext('2d')
-  controls.addEventListener('click', ButtonClick)
+  // controls.addEventListener('mousedown', ButtonMousedown)
+  controls.addEventListener('pointerdown', ButtonMousedown)
+  controls.addEventListener('mouseup', resetArrows)
+  controls.addEventListener('pointerleave', resetArrows)
 
   document.addEventListener('keyup', changeDirection) //for movements
 
@@ -187,6 +199,11 @@ function getState () {
     tale_food_pos[2] // izq
   ]
   // return np.array(state, dtype=int)
+
+  state[11] = -state[0]
+  state[12] = -state[1]
+  state[13] = -state[2]
+
   tensorA = new ort.Tensor('float32', state, [14])
   // console.log(JSON.stringify(tensorA))
   return tensorA
@@ -247,7 +264,7 @@ function update () {
     console.log('    Updating')
 
     if (gameOver) {
-      // alert('Game Over')
+      alert('Game Over')
       reset()
       return
     }
@@ -674,59 +691,139 @@ function drawControls () {
   buttonDown.arc(cx, cy, controls.width * 0.5, toRadians(45), toRadians(135))
   buttonDown.lineTo(cx, cy)
   buttonDown.closePath()
-  contextControls.fillStyle = 'white'
+  contextControls.fillStyle = 'rgb(244 87 87)'
   contextControls.fill(buttonDown)
 
   buttonLeft.moveTo(cx, cy)
   buttonLeft.arc(cx, cy, controls.width * 0.5, toRadians(135), toRadians(225))
   buttonLeft.lineTo(cx, cy)
   buttonLeft.closePath()
-  contextControls.fillStyle = 'blue'
+  contextControls.fillStyle = 'rgb(88 152 243)'
   contextControls.fill(buttonLeft)
 
   buttonUp.moveTo(cx, cy)
   buttonUp.arc(cx, cy, controls.width * 0.5, toRadians(225), toRadians(315))
   buttonUp.lineTo(cx, cy)
   buttonUp.closePath()
-  contextControls.fillStyle = 'white'
+  contextControls.fillStyle = 'rgb(82 243 98)'
   contextControls.fill(buttonUp)
 
   buttonRight.moveTo(cx, cy)
   buttonRight.arc(cx, cy, controls.width * 0.5, toRadians(315), toRadians(45))
   buttonRight.lineTo(cx, cy)
   contextControls.closePath()
-  contextControls.fillStyle = 'blue'
+  contextControls.fillStyle = 'rgb(243 208 82)'
   contextControls.fill(buttonRight)
+
+  contextControls.lineWidth = blockSize / 5
+
+  arrowUp.moveTo(cx - blockSize * 0.7, cy - blockSize)
+  arrowUp.lineTo(cx, cy - blockSize * 1.7)
+  arrowUp.lineTo(cx + blockSize * 0.7, cy - blockSize)
+  contextControls.closePath()
+  contextControls.strokeStyle = 'black'
+  contextControls.stroke(arrowUp)
+
+  arrowDown.moveTo(cx + blockSize * 0.7, cy + blockSize)
+  arrowDown.lineTo(cx, cy + blockSize * 1.7)
+  arrowDown.lineTo(cx - blockSize * 0.7, cy + blockSize)
+  contextControls.closePath()
+  contextControls.strokeStyle = 'black'
+  contextControls.stroke(arrowDown)
+
+  arrowRight.moveTo(cx + blockSize, cy + blockSize * 0.7)
+  arrowRight.lineTo(cx + blockSize * 1.7, cy)
+  arrowRight.lineTo(cx + blockSize, cy - blockSize * 0.7)
+  contextControls.closePath()
+  contextControls.strokeStyle = 'black'
+  contextControls.stroke(arrowRight)
+
+  arrowLeft.moveTo(cx - blockSize, cy + blockSize * 0.7)
+  arrowLeft.lineTo(cx - blockSize * 1.7, cy)
+  arrowLeft.lineTo(cx - blockSize, cy - blockSize * 0.7)
+  contextControls.closePath()
+  contextControls.strokeStyle = 'black'
+  contextControls.stroke(arrowLeft)
+  // arrowLeft.moveTo(cx, cy)
+  // arrowDown.moveTo(cx, cy)
+
+  // arrowRight.moveTo(cx, cy)
 }
-function ButtonClick (e) {
+function ButtonMousedown (e) {
   /** @type {CanvasRenderingContext2D} */
   var context = e.target.getContext('2d')
-  var coordX = e.offsetX
-  var coordY = e.offsetY
+  console.log(
+    'padding',
+    parseInt(
+      window.getComputedStyle(controls, null).getPropertyValue('padding-left')
+    )
+  )
+  var coordX =
+    e.offsetX -
+    parseInt(
+      window.getComputedStyle(controls, null).getPropertyValue('padding-left')
+    )
+  var coordY =
+    e.offsetY -
+    parseInt(
+      window.getComputedStyle(controls, null).getPropertyValue('padding-left')
+    )
+  // var coordX = e.offsetleft
+  // var coordY = e.offsetTop
+
   var e
 
-  if (context.isPointInPath(buttonDown, coordX, coordY)) {
+  if (
+    context.isPointInPath(buttonDown, coordX, coordY) ||
+    context.isPointInPath(arrowDown, coordX, coordY)
+  ) {
     console.log('buttonDown')
+    contextControls.strokeStyle = 'white'
+    contextControls.stroke(arrowDown)
     e.code = 'ArrowDown'
     changeDirection(e)
     return
   }
-  if (context.isPointInPath(buttonLeft, coordX, coordY)) {
+  if (
+    context.isPointInPath(buttonLeft, coordX, coordY) ||
+    context.isPointInPath(arrowLeft, coordX, coordY)
+  ) {
     console.log('buttonLeft')
+    contextControls.strokeStyle = 'white'
+    contextControls.stroke(arrowLeft)
     e.code = 'ArrowLeft'
     changeDirection(e)
     return
   }
-  if (context.isPointInPath(buttonUp, coordX, coordY)) {
+  if (
+    context.isPointInPath(buttonUp, coordX, coordY) ||
+    context.isPointInPath(arrowUp, coordX, coordY)
+  ) {
     console.log('buttonUp')
+    contextControls.strokeStyle = 'white'
+    contextControls.stroke(arrowUp)
     e.code = 'ArrowUp'
     changeDirection(e)
     return
   }
-  if (context.isPointInPath(buttonRight, coordX, coordY)) {
+  if (
+    context.isPointInPath(buttonRight, coordX, coordY) ||
+    context.isPointInPath(arrowRight, coordX, coordY)
+  ) {
     console.log('buttonRight')
+    contextControls.strokeStyle = 'white'
+    contextControls.stroke(arrowRight)
     e.code = 'ArrowRight'
     changeDirection(e)
     return
   }
+}
+
+function resetArrows () {
+  console.log('resetArrows')
+  contextControls.strokeStyle = 'black'
+  contextControls.stroke(arrowUp)
+  contextControls.stroke(arrowDown)
+  contextControls.stroke(arrowLeft)
+  contextControls.stroke(arrowRight)
 }
